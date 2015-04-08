cc450b85145889fa00f41dae0b44435574ff7e01
liyun831229 <yli0131@gmail.com>
2015-04-08 13:14:25 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,12 @@
-load('dat.RData')
+options(stringsAsFactors=F)
+dat <- read.csv('household_power_consumption.txt', sep = ';',header = T, na.strings="?", colClasses=c("character", "character", "numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dat$Date <- as.Date(dat$Date, "%d/%m/%Y")
+idx <- which(dat$Date == "2007-02-02"| dat$Date == "2007-02-01" )
+keep <- dat
+dat <- dat[idx,]
+dat$Time <- strptime(paste(dat$Date,dat$Time,sep =","),"%Y-%m-%d,%H:%M:%S")
+
+
 Sys.setlocale("LC_TIME", "English")
 png('plot4.png')
 with(dat, {
