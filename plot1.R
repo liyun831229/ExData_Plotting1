443e6b2a2bc0b62855825ef670df61a08d3ad6f9
liyun831229 <yli0131@gmail.com>
2015-04-08 13:10:54 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,22 +1,11 @@
 options(stringsAsFactors=F)
-dat <- read.csv('household_power_consumption.txt', sep = ';',
-                header = T,na.strings="?", 
-                colClasses=c("character", "character", "numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+dat <- read.csv('household_power_consumption.txt', sep = ';',header = T, na.strings="?", colClasses=c("character", "character", "numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 dat$Date <- as.Date(dat$Date, "%d/%m/%Y")
 idx <- which(dat$Date == "2007-02-02"| dat$Date == "2007-02-01" )
-#keep <- dat
+keep <- dat
 dat <- dat[idx,]
 dat$Time <- strptime(paste(dat$Date,dat$Time,sep =","),"%Y-%m-%d,%H:%M:%S")
-lapply(dat,function(x){grep("\\?",x)})
-#no missing data
-dat[,3] <- as.numeric(dat[,3])
-dat[,4] <- as.numeric(dat[,4])
-dat[,5] <- as.numeric(dat[,5])
-dat[,6] <- as.numeric(dat[,6])
-dat[,7] <- as.numeric(dat[,7])
-dat[,8] <- as.numeric(dat[,8])
-dat[,9] <- as.numeric(dat[,9])
-save('dat',file = 'dat.RData')
+
 png("plot1.png")
 hist(as.numeric(dat[,3]),xlab = 'Global Active Power(kilowatts)', main = "Global Active Power", col = 'red')
 dev.off()
