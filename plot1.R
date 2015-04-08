d2e8529ad9a55cc64fd6c8c09c2f1a2398ff6533
liyun831229 <yli0131@gmail.com>
2015-04-08 13:06:50 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,10 @@
 options(stringsAsFactors=F)
-dat <- read.csv('household_power_consumption.txt', sep = ';',header = T)
+dat <- read.csv('household_power_consumption.txt', sep = ';',
+                header = T,na.strings="?", 
+                colClasses=c("character", "character", "numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 dat$Date <- as.Date(dat$Date, "%d/%m/%Y")
 idx <- which(dat$Date == "2007-02-02"| dat$Date == "2007-02-01" )
-keep <- dat
+#keep <- dat
 dat <- dat[idx,]
 dat$Time <- strptime(paste(dat$Date,dat$Time,sep =","),"%Y-%m-%d,%H:%M:%S")
 lapply(dat,function(x){grep("\\?",x)})
