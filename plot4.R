d01f7c6007ba3cd4e0fd00a6139dc5e0f7f16766
liyun831229 <yli0131@gmail.com>
2015-04-08 12:50:35 -0400

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,16 @@
+load('dat.RData')
+Sys.setlocale("LC_TIME", "English")
+png('plot4.png')
+with(dat, {
+    par(mfrow = c(2, 2))
+    plot(Time, Global_active_power,ylab = 'Global active power',type = 'l',xlab = '')
+    plot(Time, Voltage,ylab = 'Voltage',xlab = 'datetime',type = 'l')
+    plot(Time, Sub_metering_1, yaxt = 'n', type = 'l',xlab = '',ylab = 'Energy sub metering')
+       axis(side=2, at=seq(0, 40, by=10))
+       lines(Time,Sub_metering_2,col= 'red')
+       lines(Time, Sub_metering_3, col = 'blue')
+       legend('topright', legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col = c('black','red','blue'),lty = 1)
+
+    plot(Time, Global_reactive_power, ylab = 'Global_reactive_power', xlab = 'datetime',type = 'l')
+})
+dev.off()
