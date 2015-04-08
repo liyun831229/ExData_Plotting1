cd6e198f8e2a1cbfc5a7e680ea74393d375696e2
liyun831229 <yli0131@gmail.com>
2015-04-08 12:49:26 -0400

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+load('dat.RData')
+Sys.setlocale("LC_TIME", "English")
+png("plot3.png")
+with(dat, {
+  plot(Time, Sub_metering_1, yaxt = 'n', type = 'l',xlab = '',ylab = 'Energy sub metering')
+  axis(side=2, at=seq(0, 40, by=10))
+  lines(Time,Sub_metering_2,col= 'red')
+  lines(Time, Sub_metering_3, col = 'blue')
+  legend('topright', legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col = c('black','red','blue'),lty = 1)
+})
+dev.off()
